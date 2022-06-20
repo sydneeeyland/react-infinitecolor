9a530588caeed1f915fc732c96e8379052291bf6
asd <yeye@gmail.com>
2022-06-20 17:30:43 +0800

    infinite random color

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
+let iteration = 0;
 function App() {
+  const [numbers, setNumbers] = useState([]);
+
+  useEffect(() => {
+      const interval = setInterval(() => {
+        let randColor = Math.floor(Math.random()*16777215).toString(16);
+        let item = {
+          id: iteration++,
+          value: `#${randColor}`
+        }
+        setNumbers(oldList => [...oldList, item])
+      }, 100);
+     
+      return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className='container-fluid'>
+        <div className='row col-12'>
+        {
+          numbers.map((x) => {
+            return (
+            <React.Fragment>
+              <div className='col-1 px-2 py-2'>
+                <div className='card px-4 py-4 fade-in' style={{backgroundColor: `${x.value}`}}>
+                  {x.id}
+                </div>
+              </div>
+            </React.Fragment>
+            )
+          })
+        }
+        </div>
+      </section>
+
     </div>
   );
 }
